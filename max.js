0748dfc3775ab3f6544214e7151551983ec8e1d6
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-24 16:57:34 +0600

    addad function max in js

--- max.js
+++ max.js
@@ -1,7 +1,19 @@
-const saikot = 97;
-const sumon = 89;
-if (saikot > sumon) {
-    console.log("saikot is topper in class in number");
-} else {
-    console.log("sumon is get max number in the class");
+// const saikot = 97;
+// const sumon = 89;
+// if (saikot > sumon) {
+//     console.log("saikot is topper in class in number");
+// } else {
+//     console.log("sumon is get max number in the class");
+// }
+
+function maxNumber(num1, num2) {
+    if (num1 > num2) {
+        console.log("num1 is the top number=", num1);
+        // return num1;
+    } else {
+        console.log("num2 is the max number=", num2);
+        // return num2;
+    }
 }
+const getMax = maxNumber(98, 90);
+// console.log(getMax);
