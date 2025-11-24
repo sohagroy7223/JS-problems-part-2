a2e971eb00097f358aa5e2b2e81a0e2bebc9cd05
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-24 16:42:16 +0600

    addad if else max

---
+++ max.js
@@ -0,0 +1,7 @@
+const saikot = 97;
+const sumon = 89;
+if (saikot > sumon) {
+    console.log("saikot is topper in class in number");
+} else {
+    console.log("sumon is get max number in the class");
+}
